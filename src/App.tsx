c75e8751b1f54765b5c177343bad7d3f88117e60
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-07-20 23:09:34 +0100

    feat:Major Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import MainRoutes from './MainRoutes';
 import Notification from './components/notification/Notification';
 import { ThemeProvider } from '@material-tailwind/react';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
     <ThemeProvider>
       <div className="App">
         <Notification />
-        <MainRoutes />
+        <CartProvider>
+          <MainRoutes />
+        </CartProvider>
       </div>
     </ThemeProvider>
   );
